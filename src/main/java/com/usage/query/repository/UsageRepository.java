29bd746efd90d22901858f1d741207a5ebfd9e46
taesic <xotlr333@naver.com>
2024-12-23 14:49:01 +0900

    첫번째 시도 원복

--- src/main/java/com/usage/query/repository/UsageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.usage.query.repository;
-
-import com.usage.query.entity.UsageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsageRepository extends JpaRepository<UsageEntity, Long> {
-    Optional<UsageEntity> findByUserId(String userId);
-}
--- src/main/java/com/usage/query/service/UsageQueryService.java
+++
@@ -1,7 +0,0 @@
-package com.usage.query.service;
-
-import com.usage.common.dto.UsageDTO;
-
-public interface UsageQueryService {
-    UsageDTO getUserUsage(String userId);
-}
